108f73d70ce17386ec7703c5b7f27dae83ed504f
aboo228 <avraham.s.shrem@gmail.com>
2022-09-08 14:08:09 +0300

    test

--- mini_project/model.py
+++ mini_project/model.py
@@ -12,20 +12,17 @@ train_path = r'train_df.csv'
 
 train_df = pd.read_csv(train_path)
 train_df = train_df.dropna()
-
-
-
-
-
-
-
-
+train_df = train_df.drop(['Unnamed: 0'], axis=1)
 
 
 X = train_df.drop(['Credit_Score_Standard'], axis=1)
 y = train_df['Credit_Score_Standard']
 
-x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+pca = PCA(n_components=54)
+pca.fit(X)
+X = pca.fit_transform(X)
+
+x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.1)
 
 random_forest = RandomForestClassifier(random_state=42)
 # print(cross_val_score(random_forest, X, y))
@@ -51,7 +48,7 @@ def predict_val(data, y):
         prediction.append(y_p[0])
     # print(f'{prediction}\n{list(y_val)}')
 
-    print(f'accuracy val {(prediction == y_val).sum()/len(x_val)} accuracy model {clf.best_score_}')
+    print(f'accuracy val {(prediction == y_val).sum()/len(x_val)} accuracy train {clf.best_score_}')
 
     return prediction
 
