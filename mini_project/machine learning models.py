7f925347a6ebd9f711d65abcf4239b2fb7a57f74
aboo228 <avraham.s.shrem@gmail.com>
2022-09-19 10:26:27 +0300

    test

--- mini_project/machine learning models.py
+++ mini_project/machine learning models.py
@@ -17,19 +17,14 @@ from sklearn import svm
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense,Dropout,Activation
+
 
 
 train_path = r'train_df.csv'
 
 train_df = pd.read_csv(train_path)
-train_df.drop('Customer_ID',axis=1,inplace=True)
 train_df = train_df.dropna()
+train_df = train_df.drop(['Customer_ID'], axis=1)
 
 #todo train_df without get_dummies columns
 # train_df['Credit_Score']= pd.get_dummies(train_df['Credit_Score'])['Good']
@@ -46,6 +41,14 @@ X = train_df.drop(['Credit_Score'], axis=1)
 y = train_df['Credit_Score']
 # y = pd.get_dummies(y)
 
+def predict_val(data, y):
+    prediction = []
+    for i in tqdm(range(0, len(X_val))):
+        y_p = clf.best_estimator_.predict(X[i:i+1])
+        prediction.append(y_p[0])
+    # print(f'{prediction}\n{list(y_val)}')
+
+    print(f'accuracy val {(prediction == y_val).sum()/len(X_val)} accuracy train {clf.best_score_}')
 
 # pca = PCA(n_components=54)
 # pca.fit(X)
@@ -54,27 +57,30 @@ y = train_df['Credit_Score']
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=42, shuffle=True)
 
 '''check random forest model'''
-random_forest = RandomForestClassifier(random_state=42, n_estimators=2)
-#
-# parameters = {'max_depth': [10, 8]}
-# clf = GridSearchCV(random_forest, parameters, scoring='accuracy')
-# clf.fit(X_train, y_train)
-# print(f'best_score {clf.best_score_} best_params {clf.best_params_}')
+random_forest = RandomForestClassifier(random_state=42)
+
+parameters = {'max_depth': [2, 4, 6, 8], 'n_estimators': [20, 30, 40, 50, 60]}
+clf = GridSearchCV(random_forest, parameters, scoring='accuracy')
+clf.fit(X_train, y_train)
+print(f'random_forest:  best_score {clf.best_score_} best_params {clf.best_params_}')
+predict_val(X_val, y_val)
 
 '''check  Gradient Boosting model'''
-# Gradient_Boosting = GradientBoostingClassifier(learning_rate=1.0, random_state=0')
-#
-# parameters = {'max_depth': [0, 1], 'n_estimators': [99, 100]}
-# clf = GridSearchCV(Gradient_Boosting, parameters, scoring='accuracy')
-# clf.fit(X_train, y_train)
-# print(f'best_score {clf.best_score_} best_params {clf.best_params_}')
+Gradient_Boosting = GradientBoostingClassifier(learning_rate=1.0, random_state=0)
+
+parameters = {'max_depth': [1, 2, 3, 4], 'n_estimators': [20, 30, 40, 50, 60], 'learning_rate':[0.1, 1]}
+clf = GridSearchCV(Gradient_Boosting, parameters, scoring='accuracy')
+clf.fit(X_train, y_train)
+print(f'Gradient_Boosting:  best_score {clf.best_score_} best_params {clf.best_params_}')
+predict_val(X_val, y_val)
 
 '''check adaboost'''
 adaboost = AdaBoostClassifier(random_state=0)
-parameters = {'base_estimator': [random_forest], 'n_estimators': [50], 'learning_rate': [0.3]}
+parameters = {'base_estimator': [random_forest], 'n_estimators': [20, 30, 40, 50, 60], 'learning_rate':[0.1, 1]}
 clf = GridSearchCV(adaboost, parameters, scoring='accuracy')
 clf.fit(X_train, y_train)
-
+print(f'adaboost:  best_score {clf.best_score_} best_params {clf.best_params_}')
+predict_val(X_val, y_val)
 
 '''check  SVM model'''
 # svm = svm.SVC(kernel='rbf')
@@ -89,28 +95,6 @@ clf.fit(X_train, y_train)
 # clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
 # clf.fit(X, y)
 
-# #todo NN wite keras -- first step: convert target to int
-#
-# define the keras model
-# model = Sequential()
-# model.add(Dense(100, activation='leaky_relu', input_dim=56))
-# model.add(Dropout(0.5))
-# model.add(Dense(100, activation='leaky_relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(100, activation='sigmoid'))
-# model.add(Dense(3, activation='softmax'))
-#
-# # model.add(Dense(1, activation='softmax'))
-# # compile the keras model
-# # tf.keras.optimizers.Adagrad
-# model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=0.003), metrics=['accuracy'])
-# # fit the keras model on the dataset
-# history = model.fit(X_train, y_train, epochs=20, batch_size=1000)
-# # evaluate the keras model
-# _, accuracy = model.evaluate(X_train, y_train)
-# print('Accuracy: %.2f' % (accuracy*100))
-#
-# predict = model.predict(X_val)
 
 # def predict_test(num):
 #     prediction = []
