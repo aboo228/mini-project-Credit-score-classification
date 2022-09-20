218988d50ab98f699355a48e2a690625f3e936cb
aboo228 <avraham.s.shrem@gmail.com>
2022-09-20 16:13:45 +0300

    test

--- mini_project/try_models.py
+++ mini_project/try_models.py
@@ -10,54 +10,68 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-
-
-train_df2=pd.read_csv('train_df.csv').drop('Customer_ID',axis=1)
-train_dfall=pd.read_csv('train_df.csv').drop('Customer_ID',axis=1)
+train_df2 = pd.read_csv('train_df.csv').drop('Customer_ID', axis=1)
+train_dfall = pd.read_csv('train_df.csv').drop('Customer_ID', axis=1)
 train_dfall.dropna(inplace=True)
 train_df2.dropna(inplace=True)
 
-columns_to_remove_outleirs=train_df2.columns[1:17]
-q1=None
-q3=None
-iqr=None
-upper=None
-lower=None
+columns_to_remove_outleirs = train_df2.columns[1:17]
+q1 = None
+q3 = None
+iqr = None
+upper = None
+lower = None
 
 for column in tqdm(columns_to_remove_outleirs):
-    q1=train_df2.loc[:,column].quantile(0.25)
-    q3=train_df2.loc[:, column].quantile(0.75)
-    iqr=q3-q1
-    upper=q3+1.5*iqr
-    lower=q1-1.5*iqr
-    train_df2=train_df2[train_df2.loc[:,column]<upper]
-    train_df2=train_df2[train_df2.loc[:,column]>lower]
+    q1 = train_df2.loc[:, column].quantile(0.25)
+    q3 = train_df2.loc[:, column].quantile(0.75)
+    iqr = q3 - q1
+    upper = q3 + 1.5 * iqr
+    lower = q1 - 1.5 * iqr
+    train_df2 = train_df2[train_df2.loc[:, column] < upper]
+    train_df2 = train_df2[train_df2.loc[:, column] > lower]
 
-targetall=pd.get_dummies(train_dfall.Credit_Score)
-target=pd.get_dummies(train_df2.Credit_Score)
-x_train,x_test, y_train, y_test = train_test_split(train_df2.iloc[:,1:-1],target,test_size=0.25,stratify=target, random_state=42)
-pipeline1=make_pipeline(RobustScaler(),KNeighborsClassifier(n_neighbors=9,metric= 'manhattan', weights= 'distance'))
-pipeline2=make_pipeline(StandardScaler(),KNeighborsClassifier(n_neighbors=7,metric= 'manhattan', weights= 'distance'))
-pipeline3=make_pipeline(RobustScaler(),GradientBoostingClassifier())
-acc_train=None
-acc_test=None
-acc_all=None
+targetall = pd.get_dummies(train_dfall.Credit_Score)
+target = pd.get_dummies(train_df2.Credit_Score)
+x_train, x_test, y_train, y_test = train_test_split(train_df2.iloc[:, 1:-1], target, test_size=0.25, stratify=target,
+                                                    random_state=42)
+pipeline1 = make_pipeline(RobustScaler(), KNeighborsClassifier(n_neighbors=9, metric='manhattan', weights='distance'))
+pipeline2 = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=7, metric='manhattan', weights='distance'))
+pipeline3 = make_pipeline(RobustScaler(), GradientBoostingClassifier())
+acc_train = None
+acc_test = None
+acc_all = None
 
-
-for i, pipe in tqdm(enumerate([pipeline1, pipeline2, pipeline3])):
-
-    if i+1==3:
-    pipe.fit(x_train, y_train)
-    acc_train=(np.argmax(pipe.predict(x_train),axis=1)==np.argmax(y_train.to_numpy(),axis=1)).sum()/ y_train.shape[0]
-    acc_test=(np.argmax(pipe.predict(x_test),axis=1)==np.argmax(y_test.to_numpy(),axis=1)).sum()/ y_test.shape[0]
-    acc_all=(np.argmax(pipe.predict(train_dfall.iloc[:,1:-1]),axis=1)==np.argmax(targetall.to_numpy(),axis=1)).sum()/ targetall.shape[0]
+for pipe in tqdm([pipeline1, pipeline2]):
+    pipe .fit(x_train, y_train)
+    acc_train = (np.argmax(pipe.predict(x_train), axis=1) == np.argmax(y_train.to_numpy(), axis=1)).sum()/y_train.shape[0]
+    acc_test = (np.argmax(pipe.predict(x_test), axis=1) == np.argmax(y_test.to_numpy(), axis=1)).sum() / y_test.shape[0]
+    acc_all = (np.argmax(pipe.predict(train_dfall.iloc[:, 1:-1]), axis=1) == np.argmax(targetall.to_numpy(),axis=1)).sum() / targetall.shape[0]
     print(f'acc_train:{acc_train}\nacc_test:{acc_test}\nacc_all{acc_all}')
+
+
+##################
+convert_dict = {'Poor': 0, 'Standard': 1, 'Good': 2}
+for (label, num) in convert_dict.items():
+    train_df2.loc[train_df2.index[train_df2.loc[:, 'Credit_Score'] == label], 'Credit_Score'] = float(num)
+
+targetall = train_dfall.Credit_Score
+target = train_df2.Credit_Score
+x_train, x_test, y_train, y_test = train_test_split(train_df2.iloc[:, 1:-1], target, test_size=0.25, stratify=target,
+                                                    random_state=42)
+
+pipeline3 .fit(x_train, y_train)
+acc_train = (np.argmax(pipeline3.predict(x_train), axis=1) == np.argmax(y_train.to_numpy(), axis=1)).sum() / y_train.shape[0]
+acc_test = (np.argmax(pipeline3.predict(x_test), axis=1) == np.argmax(y_test.to_numpy(), axis=1)).sum() / y_test.shape[0]
+acc_all = (np.argmax(pipeline3.predict(train_dfall.iloc[:, 1:-1]), axis=1) == np.argmax(targetall.to_numpy(),axis=1)).sum() / targetall.shape[0]
+print(f'acc_train:{acc_train}\nacc_test:{acc_test}\nacc_all{acc_all}')
+##################
 
 from scipy import stats
 
-def models_votes(x,target):
-    pip1=np.argmax(pipeline1.predict(x),axis=1)
-    pip2=np.argmax(pipeline2.predict(x),axis=1)
-    pip3=np.argmax(pipeline3.predict(x),axis=1)
-    votes=np.max(stats.mode(np.array([pip1,pip2,pip3]))[0])
+
+def models_votes(x, target):
+    pip1 = np.argmax(pipeline1.predict(x), axis=1)
+    pip2 = np.argmax(pipeline2.predict(x), axis=1)
+    pip3 = np.argmax(pipeline3.predict(x), axis=1)
+    votes = np.max(stats.mode(np.array([pip1, pip2, pip3]))[0])
